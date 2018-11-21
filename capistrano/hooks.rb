e15a618e23e5058df09547f5eb82a60670711ac1
Melvin <melvinatieno@gmail.com>
2018-11-21 13:26:51 +0300

    update make figaro task run before capistrano's migration task

--- capistrano/hooks.rb
+++ capistrano/hooks.rb
@@ -7,10 +7,10 @@ before 'deploy:updated', 'puma:config'
 before 'deploy:starting', 'check:revision'
 before 'deploy:starting', 'check:first_deploy'
 after 'deploy:publishing', 'deploy:restart'
-after "deploy:publishing", "sidekiq:restart"  
+after 'deploy:publishing', 'sidekiq:restart'  
 # after 'deploy:migrate', 'deploy:seed'
-after "deploy:symlink:release", "figaro:setup"
-after "figaro:setup", "figaro:symlink"
+after 'figaro:setup', 'figaro:symlink'
+before 'deploy:migrate', 'figaro:setup'
 
 # cap [ENV] deploy:assets_compile
 after 'deploy:assets_compile', 'deploy:restart'
